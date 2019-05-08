1c3ebf674aa53aa0269ae612abee443c463048cd
Erika Pauwels <erika.pauwels@gmail.com>
2019-05-08 18:08:33 +0200

    Basic styling using Semantic-UI

---
+++ app/components/docs-menu.js
@@ -0,0 +1,135 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+
+export default class DocsMenuComponent extends Component {
+  @tracked core = [
+    {
+      label: 'RDFa editor',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor'
+    }
+  ];
+
+  @tracked plugins = [
+    {
+      label: '',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor'
+    },
+    {
+      label: 'Aanwezigen',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-aanwezigen-plugin'
+    },
+    {
+      label: 'Agenda',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-agenda-plugin'
+    },
+    {
+      label: 'Aanstelling BCSD',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-bijzonder-comite-aanstelling-plugin'
+    },
+    {
+      label: 'Citaten',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-citaten-plugin'
+    },
+    {
+      label: 'Console logger',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-console-logger-plugin'
+    },
+    {
+      label: 'Date manipulation',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-date-manipulation-plugin'
+    },
+    {
+      label: 'Date overwrite',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-date-overwrite-plugin'
+    },
+    {
+      label: 'Date',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-date-plugin'
+    },
+    {
+      label: 'Document tasklist',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-document-tasklist-plugin'
+    },
+    {
+      label: 'Document title',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-document-title-plugin'
+    },
+    {
+      label: 'Fractievorming',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-fractievorming-plugin'
+    },
+    {
+      label: 'Aanstelling gemeenteraadsleden',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-gemeenteraadsleden-aanstelling-plugin'
+    },
+    {
+      label: 'Generic model',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-generic-model-plugin'
+    },
+    {
+      label: 'Installatievergadering',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-installatievergadering-plugin'
+    },
+    {
+      label: 'Mandaat',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-mandaat-plugin'
+    },
+    {
+      label: 'Mandataris',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-mandataris-plugin'
+    },
+    {
+      label: 'Personen',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-personen-plugin'
+    },
+    {
+      label: 'Aanstelling schepenen',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-schepenen-aanstelling-plugin'
+    },
+    {
+      label: 'Scoped bestuursorgaan',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-scoped-bestuursorgaan-plugin'
+    },
+    {
+      label: 'Scroll to',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-scroll-to-plugin'
+    },
+    {
+      label: 'Template',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-standard-template-plugin'
+    },
+    {
+      label: 'Stemming',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-stemming-module-plugin'
+    },
+    {
+      label: 'Variables',
+      namespace: 'lblod',
+      repository: 'ember-rdfa-editor-template-variables-manager-plugin'
+    }
+  ];
+}
--- tests/integration/components/docs-menu-test.js
+++ tests/integration/components/docs-menu-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | main-menu', function(hooks) {
+module('Integration | Component | docs-menu', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<MainMenu />`);
+    await render(hbs`<DocsMenu />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      <MainMenu>
+      <DocsMenu>
         template block text
-      </MainMenu>
+      </DocsMenu>
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
