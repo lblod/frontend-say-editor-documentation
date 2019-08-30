8a5061a80b1a4916ab74a5f4e7d2528f315cb8ee
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-08-30 13:44:43 +0200

    bulk commit

--- app/routes/docs/deploy-as-addon.js
+++ app/routes/docs/deploy-as-addon.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class DocsDeployAsAddonRoute extends Route {
+  @service repository;
+
+  async model(){
+    return this.repository.fetchReadme('lblod', 'say-editor-as-addon-tutorial');
+  }
 }
--- app/routes/docs/deploy-as-library.js
+++ app/routes/docs/deploy-as-library.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class DocsDeployAsLibraryRoute extends Route {
+  @service repository;
+
+  async model(){
+    return this.repository.fetchReadme('lblod', 'frontend-embeddable-notule-editor');
+  }
 }
