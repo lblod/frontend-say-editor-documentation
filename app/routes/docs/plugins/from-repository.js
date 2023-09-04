48734a35385cc17d9a84959d2c0354f533a6013d
Niels Vandekeybus <progster@gmail.com>
2023-09-04 15:41:05 +0200

    limit information to up to date info

---
+++ app/routes/docs/plugins/from-repository.js
@@ -0,0 +1,10 @@
+import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+
+export default class DocsPluginsFromRepositoryRoute extends Route {
+  @service repository;
+
+  async model({namespace, repository}){
+    return this.repository.fetchReadme(namespace, repository);
+  }
+}
